6bf1adf3520d9b4b14a8cad0e052ba8091cfb0fd
ailyahjo <jordan.aliy@northeastern.edu>
2024-04-17 15:24:02 -0400

    restarting

--- Users/model.js
+++
@@ -1,5 +0,0 @@
-// This is the mongoose model for the users collection
-import mongoose from 'mongoose';
-import schema from './schema.js';
-const model = mongoose.model('UserModel', schema);
-export default model;
