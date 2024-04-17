207208f6f8031bacd9c7d90dc4860f27389af4d6
ailyahjo <jordan.aliy@northeastern.edu>
2024-04-17 15:24:17 -0400

    restarting

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 // Server App.js
 
 import express from 'express';
-import mongoose from 'mongoose';
-import UserRoutes from './Users/routes.js';
 import Hello from './Hello.js';
 import Lab5 from "./Lab5.js";
 import cors from 'cors';
@@ -10,11 +8,9 @@ import CourseRoutes from './Courses/routes.js';
 import ModuleRoutes from './Modules/routes.js';
 import AssignmentRoutes from './Assignments/routes.js';
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json()); 
-UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
