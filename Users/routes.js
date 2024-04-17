e865da3ac821e33b9dd920e5317a950b2db565a0
ailyahjo <jordan.aliy@northeastern.edu>
2024-04-17 19:49:30 -0400

    3.5.4:  Finding all users implementation

--- Users/routes.js
+++ Users/routes.js
@@ -1,24 +1,36 @@
 // Routes for the user collection
-import * as dao from "./doa.js";
+import * as dao from "./dao.js";
 let currentUser = null;
 export default function UserRoutes(app) {
+  //  Create user
   const createUser = async (req, res) => { };
+  // Delete user
   const deleteUser = async (req, res) => { };
-  const findAllUsers = async (req, res) => { };
+  // Find all users
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+  app.get("/api/users", findAllUsers);  // Find user by id
   const findUserById = async (req, res) => { };
+  // Update user
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
     currentUser = await dao.findUserById(userId);
     res.json(status);
   };
+  // Sign up
   const signup = async (req, res) => { };
+  // Sign in
   const signin = async (req, res) => {
     const { username, password } = req.body;
     currentUser = await dao.findUserByCredentials(username, password);
     res.json(currentUser);
    };
+  //  Sign out
   const signout = (req, res) => { };
+  // Profile
   const profile = async (req, res) => {
     res.json(currentUser);
   };
