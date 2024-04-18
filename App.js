c466c9ffd8675c4e7ac4f3feba25e1c5e81e4a49
ailyahjo <jordan.aliy@northeastern.edu>
2024-04-18 12:07:27 -0400

    fixing

--- App.js
+++ App.js
@@ -9,32 +9,10 @@ import ModuleRoutes from './Modules/routes.js';
 import AssignmentRoutes from './Assignments/routes.js';
 import mongoose from 'mongoose';
 import UserRoutes from './Users/routes.js';
-import session from "express-session";
-import "dotenv/config";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
-    })
-   );   
-const sessionOptions = {
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    //   domain: "https://kanbas-node-server-app-jg2y.onrender.com",
-    };
-  }
-  app.use(session(sessionOptions)); 
+app.use(cors());
 app.use(express.json()); 
 UserRoutes(app);
 CourseRoutes(app);
